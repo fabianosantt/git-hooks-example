995afaa90172fbc449a5062666cf11c1bfdde68d
Fabiano Silva <fabianosilva@Fabianos-MacBook-Pro.local>
2020-10-16 21:54:34 -0300

    teste

--- tests/SomeLibTests/UnitTest1.cs
+++ tests/SomeLibTests/UnitTest1.cs
@@ -9,7 +9,7 @@ namespace SomeLibTests
         public void Test1()
         {
             // comment in test
-            int expteced = 3;
+            int expteced = 4;
             int actual = 1;
             Assert.Equal(expteced, actual);
         }
